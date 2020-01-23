00a3169f20a5523693d08097a2eb3312e866893b
imdodcc13 <imdodcc12@gmail.com>
2020-01-23 17:48:29 -0300

    act

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-document.getElementById("senate-data").innerHTML = JSON.stringify(datos,null,2);
+let table = document.getElementById("senate-data");
+let arrayMembers = datos.results[0].members;
 
+function nullToString(input){
+    if(input == null){
+        return "...";
+    }
+    return input;
+}
+
+let tbody = "<tbody>"
+for(let i = 0; i < arrayMembers.length; i++){
+
+    tbody += "<tr><td><a href="+nullToString(arrayMembers[i].url)+" target='_blank'>"+nullToString(arrayMembers[i].last_name)+"</td>";
+    tbody += "<td>"+nullToString(arrayMembers[i].first_name)+"</td>";
+    tbody += "<td>"+nullToString(arrayMembers[i].middle_name)+"</td>";
+    tbody += "<td>"+nullToString(arrayMembers[i].party)+"</td>";
+    tbody += "<td>"+nullToString(arrayMembers[i].state)+"</td>";
+    tbody += "<td>"+nullToString(arrayMembers[i].seniority)+"</td>";
+    tbody += "<td>"+nullToString(arrayMembers[i].votes_with_party_pct)+"%</td></tr>";
+}
+tbody += "<tbody>"
+table.innerHTML += tbody;
