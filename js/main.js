fd0462e4edec2999f60682e7991d845e52a5348d
21Diego <morinigodiego21@gmail.com>
2020-01-27 17:49:03 -0300

    tabla

--- js/main.js
+++ js/main.js
@@ -5,13 +5,22 @@ function nullToString(input){
     return input;
 }
 
+let states = [];
+let parties = [];
+
 function createTbody(arrayInput){
     let tbody = "<tbody>"
     for(let i = 0; i < arrayInput.length; i++){
         tbody += "<tr><td>"+(i + 1)+"</td>";
         tbody += "<td><a href="+nullToString(arrayInput[i].url)+" target='_blank'>"+nullToString(arrayInput[i].last_name)+" "+nullToString(arrayInput[i].first_name)+" "+nullToString(arrayInput[i].middle_name)+"</a></td>";
         tbody += "<td>"+nullToString(arrayInput[i].party)+"</td>";
+        if(parties.indexOf(arrayInput[i].party) == -1){
+            parties.push(arrayInput[i].party);
+        }
         tbody += "<td>"+nullToString(arrayInput[i].state)+"</td>";
+        if(states.indexOf(arrayInput[i].state) == -1){
+            states.push(arrayInput[i].state);
+        }
         tbody += "<td>"+nullToString(arrayInput[i].seniority)+"</td>";
         tbody += "<td>"+nullToString(arrayInput[i].votes_with_party_pct)+"%</td></tr>";
     }
@@ -21,12 +30,44 @@ function createTbody(arrayInput){
 let tableSenate = document.getElementById("senate-data");
 let tableHouse = document.getElementById("house-data");
 
-if(typeof senateData !== "undefined"){
-    let arrayMembers = senateData.results[0].members;
+let arrayMembers = data.results[0].members;
+if(tableSenate != null){
     tableSenate.innerHTML += createTbody(arrayMembers);
 }
-else if(typeof houseData !== "undefined"){
-    let arrayMembers = houseData.results[0].members;
+if(tableHouse != null){
     tableHouse.innerHTML += createTbody(arrayMembers);
 }
+
+
+let select = document.getElementById("states-select");
+let option;
+for(let i = 0; i < states.length; i++){
+    option = document.createElement("option");
+    option.setAttribute("value", states[i]);
+    option.innerHTML = states[i];
+    select.appendChild(option);
+}
+
+console.log(parties);
+console.log(states);
 // Holaaa dieguitooo <]:{V
+
+function createTbody2(arrayInput){
+    let tbody = "<tbody>"
+    for(let i = 0; i < arrayInput.length; i++){
+        tbody += "<tr><td>"+(i + 1)+"</td>";
+        tbody += "<td><a href="+nullToString(arrayInput[i].url)+" target='_blank'>"+nullToString(arrayInput[i].last_name)+" "+nullToString(arrayInput[i].first_name)+" "+nullToString(arrayInput[i].middle_name)+"</a></td>";
+        tbody += "<td>"+nullToString(arrayInput[i].party)+"</td>";
+        if(parties.indexOf(arrayInput[i].party) == -1){
+            parties.push(arrayInput[i].party);
+        }
+        tbody += "<td>"+nullToString(arrayInput[i].state)+"</td>";
+        if(states.indexOf(arrayInput[i].state) == -1){
+            states.push(arrayInput[i].state);
+        }
+        tbody += "<td>"+nullToString(arrayInput[i].seniority)+"</td>";
+        tbody += "<td>"+nullToString(arrayInput[i].votes_with_party_pct)+"%</td></tr>";
+    }
+    tbody += "<tbody>"
+    return tbody;
+}
--- js/pro-congress-113-house.js
+++ js/pro-congress-113-house.js
@@ -1,4 +1,4 @@
-var houseData = {
+var data = {
     "status":"OK",
     "copyright":" Copyright (c) 2020 Pro Publica Inc. All Rights Reserved.",
     "results":[
