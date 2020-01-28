e0580257323dd69866b73a0b32ea8f387381b120
21Diego <morinigodiego21@gmail.com>
2020-01-28 17:28:13 -0300

    act

--- js/main.js
+++ js/main.js
@@ -5,8 +5,22 @@ function nullToString(input){
     return input;
 }
 
+let table = document.querySelector("#table-data");
+let tbody = document.querySelector("tbody");
+console.log(tbody);
+let members = data.results[0].members;
+
+
+members.forEach(member => {
+    let row = tbody.insertRow(-1);
+    row.innerHTML = `<td><a href="${member.url} target="_blank">${member.last_name}, ${member.first_name} ${nullToString(member.middle_name)}</a></td>`
+    +`<td>${member.party}</td><td>${member.state}</td><td>${member.seniority}`
+    +`<td>${member.votes_with_party_pct}%</td>`;
+});
 let states = [];
 let parties = [];
+
+
 
 function createTbody(arrayInput){
     let tbody = "<tbody>"
@@ -27,17 +41,10 @@ function createTbody(arrayInput){
     tbody += "<tbody>"
     return tbody;
 }
-let tableSenate = document.getElementById("senate-data");
-let tableHouse = document.getElementById("house-data");
 
-let arrayMembers = data.results[0].members;
-if(tableSenate != null){
-    tableSenate.innerHTML += createTbody(arrayMembers);
-}
-if(tableHouse != null){
-    tableHouse.innerHTML += createTbody(arrayMembers);
-}
 
+
+table.innerHTML += createTbody(arrayMembers);
 
 let select = document.getElementById("states-select");
 let option;
@@ -52,22 +59,5 @@ console.log(parties);
 console.log(states);
 // Holaaa dieguitooo <]:{V
 
-function createTbody2(arrayInput){
-    let tbody = "<tbody>"
-    for(let i = 0; i < arrayInput.length; i++){
-        tbody += "<tr><td>"+(i + 1)+"</td>";
-        tbody += "<td><a href="+nullToString(arrayInput[i].url)+" target='_blank'>"+nullToString(arrayInput[i].last_name)+" "+nullToString(arrayInput[i].first_name)+" "+nullToString(arrayInput[i].middle_name)+"</a></td>";
-        tbody += "<td>"+nullToString(arrayInput[i].party)+"</td>";
-        if(parties.indexOf(arrayInput[i].party) == -1){
-            parties.push(arrayInput[i].party);
-        }
-        tbody += "<td>"+nullToString(arrayInput[i].state)+"</td>";
-        if(states.indexOf(arrayInput[i].state) == -1){
-            states.push(arrayInput[i].state);
-        }
-        tbody += "<td>"+nullToString(arrayInput[i].seniority)+"</td>";
-        tbody += "<td>"+nullToString(arrayInput[i].votes_with_party_pct)+"%</td></tr>";
-    }
-    tbody += "<tbody>"
-    return tbody;
-}
+
+/******************************/
