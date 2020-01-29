dad2ae07fbdf27c8ad92d14534c4ee3132d1eaa4
21Diego <morinigodiego21@gmail.com>
2020-01-29 17:48:35 -0300

    filtrado

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-function nullToString(input){
-    if(input == null){
-        return "";
+function changeString(stringIn, compare, replace){
+    if(stringIn == compare){
+        return replace;
     }
-    return input;
+    return stringIn;
 }
+
 
 let table = document.querySelector("#table-data");
 let tbody = document.querySelector("tbody");
@@ -11,31 +12,44 @@ let tbody = document.querySelector("tbody");
 let members = data.results[0].members;
 let states = [];
 
-
-
-members.forEach(member => {
-    let row = tbody.insertRow(-1);
-    row.innerHTML = `<td><a href="${member.url} target="_blank">${member.last_name}, ${member.first_name} ${nullToString(member.middle_name)}</a></td>`
-    +`<td>${member.party}</td><td>${member.state}</td><td>${member.seniority}`
-    +`<td>${member.votes_with_party_pct}%</td>`;
-    if(states.indexOf(member.state) == -1){
-        states.push(member.state);
-    }
-});
-
-let select = document.getElementById("states-select");
-let option;
-for(let i = 0; i < states.length; i++){
-    option = document.createElement("option");
-    option.setAttribute("value", states[i]);
-    option.innerHTML = states[i];
-    select.appendChild(option);
-}
-
-console.log(document.querySelectorAll(".filtro"));
-
-
-// Holaaa dieguitooo <]:{V
-
-
-/******************************/
+function filtrar(){
+    tbody.innerHTML = "";
+    let partiesCheck=document.querySelectorAll(".parties:checked");
+    let select=document.querySelector("#states-select");
+    
+    partiesCheck.forEach(check => 
+        members.filter(member => 
+            member.party === check.value && (member.state === select.value || select.value === "all")).map(member => {
+                let row = tbody.insertRow(-1);
+                row.innerHTML = `<td><a href="${changeString(member.url,"","#")}" target="_blank">${member.last_name}, ${member.first_name} ${changeString(member.middle_name,null,"")}</a></td>`
+                +`<td>${member.party}</td><td>${member.state}</td><td>${member.seniority}`
+                +`<td>${member.votes_with_party_pct}%</td>`;
+                if(states.indexOf(member.state) == -1){
+                    states.push(member.state);
+                }
+            })
+            )   
+            let option;
+            select.innerHTML = "<option value='all'>all</option>";
+            for(let i = 0; i < states.length; i++){
+                option = document.createElement("option");
+                option.setAttribute("value", states[i]);
+                option.innerHTML = states[i];
+                select.appendChild(option);
+            }
+            
+        }
+        
+        
+        document.querySelector("#rep").addEventListener("click",filtrar);
+        document.querySelector("#dem").addEventListener("click",filtrar);
+        document.querySelector("#ind").addEventListener("click",filtrar);
+        document.querySelector("#states-select").addEventListener("change",filtrar);
+        filtrar();
+        
+        // Holaaa dieguitooo <]:{V
+        
+        
+        /******************************/
+        
+        
