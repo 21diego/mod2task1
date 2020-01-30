a3b1fba8e79e9ea9bf2c3bee58d336bbff2446b1
21Diego <morinigodiego21@gmail.com>
2020-01-30 16:10:19 -0300

    finalizacion de filtros

--- js/main.js
+++ js/main.js
@@ -5,6 +5,19 @@ function changeString(stringIn, compare, replace){
     return stringIn;
 }
 
+statesFull = {
+    AL: "Alabama",AK: "Alaska",AZ: "Arizona",AR: "Arkansas",CA: "California",CO: "Colorado",
+    CT: "Connecticut",DE: "Delaware",DC: "Distrito de Columbia",FL: "Florida",GA: "Georgia",
+    HI: "Hawaii",ID: "Idaho",IL: "Illinois",IN: "Indiana",IA: "Iowa",KS: "Kansas",
+    KY: "Kentucky",LA: "Luisiana",ME: "Maine",MD: "Maryland",MA: "Massachusetts",
+    MI: "Míchigan",MN: "Minnesota",MS: "Misisipi",MO: "Misuri",MT: "Montana",NE: "Nebraska",
+    NV: "Nevada",NH: "Nuevo Hampshire",NJ: "Nueva Jersey",NM: "Nuevo México",
+    NY: "Nueva York",NC: "Carolina del Norte",ND: "Dakota del Norte",OH: "Ohio",
+    OK: "Oklahoma",OR: "Oregón",PA: "Pensilvania",RI: "Rhode Island",SC: "Carolina del Sur",
+    SD: "Dakota del Sur",TN: "Tennessee",TX: "Texas",UT: "Utah",VT: "Vermont",VA: "Virginia",
+    WA: "Estado de Washington",WV: "Virginia Occidental",WI: "Wisconsin",WY: "Wyoming"
+}
+
 
 let table = document.querySelector("#table-data");
 let tbody = document.querySelector("tbody");
@@ -16,16 +29,17 @@ function filtrar(){
     tbody.innerHTML = "";
     let partiesCheck=document.querySelectorAll(".parties:checked");
     let select=document.querySelector("#states-select");
-    
+    let value = select.value;
+    states = [];
     partiesCheck.forEach(check => 
         members.filter(member => 
-            member.party === check.value && (member.state === select.value || select.value === "all")).map(member => {
+            member.party === check.value && (statesFull[member.state] === value || value === "all")).forEach(member => {
                 let row = tbody.insertRow(-1);
-                row.innerHTML = `<td><a href="${changeString(member.url,"","#")}" target="_blank">${member.last_name}, ${member.first_name} ${changeString(member.middle_name,null,"")}</a></td>`
+                row.innerHTML = `<td><a href="${changeString(member.url,"","#")}">${member.last_name}, ${member.first_name} ${changeString(member.middle_name,null,"")}</a></td>`
                 +`<td>${member.party}</td><td>${member.state}</td><td>${member.seniority}`
                 +`<td>${member.votes_with_party_pct}%</td>`;
-                if(states.indexOf(member.state) == -1){
-                    states.push(member.state);
+                if(states.indexOf(statesFull[member.state]) == -1){
+                    states.push(statesFull[member.state]);
                 }
             })
             )   
@@ -37,6 +51,7 @@ function filtrar(){
                 option.innerHTML = states[i];
                 select.appendChild(option);
             }
+            select.value = value;
             
         }
         
